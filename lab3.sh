cfec278760618949a76264a0fb116ce4d534d6f6
vdmfernandes <vdmfernandes@gmail.com>
2016-12-18 17:36:15 -0300

    Update lab3.sh

--- lab3.sh
+++ lab3.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 sudo apt-get update
+sudo apt-get install -y python-pip python-dev build-essential 
 sudo apt-get install -y arduino arduino-core 
 sudo apt-get install -y git-all
+
+sudo pip install pyserial
 
 cd /usr/share/arduino/libraries/
 sudo git clone https://github.com/markruys/arduino-DHT.git
